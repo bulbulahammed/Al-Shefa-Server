39e8974833af1b89dd6a9dbc7395d61df08053c1
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-24 18:52:29 +0600

    Mongo db connection process start

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://AlShefa_admin:<password>@cluster0.k2xq3je.mongodb.net/?retryWrites=true&w=majority";
+
 
 app.get('/', (req, res) => {
   res.send('Hello from Al-Shefa Server!')
