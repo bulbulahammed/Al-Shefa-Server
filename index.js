d4b8fef924d8ef93a5d02e350646b32779829a9b
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-17 13:51:34 +0600

    Limit Dashboard features based on admin level access

--- index.js
+++ index.js
@@ -85,16 +85,30 @@ async function run(){
     res.send(users);
   })
 
-// Make Admin
+// Make Someone an Admin
 app.put('/user/admin/:email',verifyJWT, async(req,res)=>{  
   const email = req.params.email;
-  const filter = {email:email};
-  const updateDoc ={
-    $set:{role:'admin'},
+  const requester = req.decoded.email;
+  const requesterAccount = await userCollection.findOne({email:requester});
+  if(requesterAccount.role === 'admin'){
+    const filter = {email:email};
+    const updateDoc ={
+      $set:{role:'admin'},
+    };
+    const result = await userCollection.updateOne(filter,updateDoc);
+    res.send({result});
+  }
+  else{
+    res.status(403).send({message: 'forbidden'});
+  }
+})
 
-  };
-  const result = await userCollection.updateOne(filter,updateDoc);
-  res.send({result});
+// Get Admin
+app.get('/admin/:email', async(req,res)=>{
+    const email = req.params.email;
+    const user = await userCollection.findOne({email: email});
+    const isAdmin = user.role === 'admin';
+    res.send({admin: isAdmin})
 })
 
   // Send/Update user Data in Database
