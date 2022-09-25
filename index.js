64cadcc444ae64b9f1ca7a800b4ad14a6eaf2cd5
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-25 16:01:42 +0600

    VerifyAdmin middleware and load all doctors

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ async function run(){
       const bookingCollection = client.db("Al-Shefa").collection("bookings");
       const userCollection = client.db("Al-Shefa").collection("users");
       const doctorCollection = client.db("Al-Shefa").collection("doctors");
+
+      // Verify Admin
+      const verifyAdmin = async(req,res,next) =>{
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({email:requester});
+        if(requesterAccount.role === 'admin'){
+          next();
+        }
+        else{
+          res.status(403).send({message:'Forbidden Access'});
+        }
+      }
      
 
     // Get data According to User
@@ -85,21 +97,14 @@ async function run(){
       });
 
     // Make Someone an Admin
-    app.put('/user/admin/:email',verifyJWT, async(req,res)=>{  
+    app.put('/user/admin/:email',verifyJWT,verifyAdmin, async(req,res)=>{  
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({email:requester});
-      if(requesterAccount.role === 'admin'){
         const filter = {email:email};
         const updateDoc ={
           $set:{role:'admin'},
         };
         const result = await userCollection.updateOne(filter,updateDoc);
         res.send({result});
-      }
-      else{
-        res.status(403).send({message: 'forbidden'});
-      }
     });
 
     // Get Admin
@@ -150,8 +155,14 @@ async function run(){
         res.send(services);
       });
 
+      // Get All the Doctor
+      app.get('/doctor',verifyJWT,verifyAdmin,async(req,res)=>{
+        const doctors = await doctorCollection.find().toArray();
+        res.send(doctors);
+      });
+
       //Post doctor data to database
-      app.post('/doctor',verifyJWT, async(req,res)=>{
+      app.post('/doctor',verifyJWT,verifyAdmin, async(req,res)=>{
         const doctor = req.body;
         const result = await doctorCollection.insertOne(doctor);
         res.send(result);
